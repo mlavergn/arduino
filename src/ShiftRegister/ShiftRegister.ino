15874beee80c9b288b3c8a7d7a6c459f2e57b208
Marc Lavergne <mlavergn@gmail.com>
2018-02-25 00:07:01 -0800

    Add: Shift register example

---
+++ src/ShiftRegister/ShiftRegister.ino
@@ -0,0 +1,39 @@
+/*
+Shift Register
+
+- 1x Shift register (74HC595)
+- 15x male:male wires
+- 8x 220 ohm resistors
+- 8 LEDs
+*/
+
+#include <Arduino.h>
+
+int latchPin = 5;
+int clockPin = 6;
+int dataPin = 4;
+
+byte leds = 0;
+
+void setup() {
+    pinMode(latchPin, OUTPUT);
+    pinMode(dataPin, OUTPUT);
+    pinMode(clockPin, OUTPUT);
+}
+
+void loop() {
+    leds = 0;
+    updateShiftRegister();
+    delay(500);
+    for (int i = 0; i < 8; i++) {
+        bitSet(leds, i);
+        updateShiftRegister();
+        delay(500);
+    }
+}
+
+void updateShiftRegister() {
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, leds);
+    digitalWrite(latchPin, HIGH);
+}
